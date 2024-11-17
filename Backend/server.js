aa71f7bab32eb3420c99b31fc82dc45fba4f3924
Keshav Chandel <chandelkeshav4@gmail.com>
2024-11-17 22:25:53 +0530

    added backend

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+const PORT = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('Backend is running');
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
