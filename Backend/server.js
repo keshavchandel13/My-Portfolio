53a48c5954075fb49de3c9c1046effa159967521
Keshav Chandel <chandelkeshav4@gmail.com>
2024-11-17 22:42:47 +0530

    .

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,14 @@ var cors = require('cors')
 var app = express()
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-    origin: 'https://my-portfolio-ljg7.vercel.app/' // Replace with your frontend's URL during production
+    origin: 'https://my-portfolio-ljg7.vercel.app/' 
   }));
 app.use(express.json())
+app.get('/your-endpoint', (req, res) => {
+    res.json({ message: 'Data fetched successfully!' });
+    console.log("jai ho");
+  });
+  
 app.get('/', (req, res) => {
     res.send('Backend is running');
   });
